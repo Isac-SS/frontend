f2a88d774f61825bb9bfee89ba257e39cf372c28
Isac-SS <isacsants679@gmail.com>
2023-09-10 21:41:59 -0300

    feat: criação do componente home

--- src/app/home/home-routing.module.ts
+++ src/app/home/home-routing.module.ts
@@ -1,12 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { HomePage } from './home.page';
+import { HomeComponent } from './home.page';
 
 const routes: Routes = [
-  {
-    path: '',
-    component: HomePage,
-  }
+  { path: '', component: HomeComponent },
+
 ];
 
 @NgModule({
--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,12 +1,22 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
 })
-export class HomePage {
+export class HomeComponent {
 
-  constructor() {}
+  constructor(private router: Router) {}
 
+  navigateTo(page: string) {
+    if (page === 'atletas') {
+      this.router.navigate(['/atletas']);
+    } else if (page === 'eventos') {
+      this.router.navigate(['/eventos']);
+    } else if (page === 'embarcacoes') {
+      this.router.navigate(['/embarcacoes']);
+    }
+  }
 }
