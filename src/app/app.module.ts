a041e3248bf641eb4dec7cadf977c0201c7aed8d
Isac-SS <isacsants679@gmail.com>
2023-09-27 14:07:13 -0300

    build: instalação do angular material

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,13 +9,26 @@ import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
 import { AtletasService } from './shared/services/atletas/atletas.service';
 
+//material
+import { MatToolbarModule } from '@angular/material/toolbar';
+
+//components
+import { HomeComponent } from './components/home/home.component';
+
+
 @NgModule({
-  declarations: [AppComponent],
+  declarations: [
+    AppComponent,
+    HomeComponent
+  ],
   imports: [
     HttpClientModule,
     BrowserModule,
     IonicModule.forRoot(),
-    AppRoutingModule
+    AppRoutingModule,
+
+    //Material
+    MatToolbarModule
   ],
   providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy }, AtletasService ],
   bootstrap: [AppComponent],
