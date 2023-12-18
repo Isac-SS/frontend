c769674254b5567ec613c3622fdf4a92a886267d
Isac Santos de Sá <isacsants679@gmail.com>
2023-12-18 16:47:39 -0300

    feat - criando eventos

---
+++ src/app/components/eventos/lista-eventos/lista-eventos.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-lista-eventos',
+  templateUrl: './lista-eventos.component.html',
+  styleUrls: ['./lista-eventos.component.scss'],
+})
+export class ListaEventosComponent  implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {}
+
+}
